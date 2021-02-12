789c57364b97bb1beaa641ec839bf0768d48b205
Yoann LE PECH <51167167+Yoannlp44@users.noreply.github.com>
2021-02-12 19:19:10 +0100

    add enemy

---
+++ enemy 1/Enemy 1.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+const GRAVITY = 10
+const SPEED = 30
+const FLOOR = Vector2(0, -1)
+var velocity = Vector2()
+var direction = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	velocity.x = SPEED * direction
+	if direction == 1:
+		$AnimatedSprite.flip_h = false
+	else:
+		$AnimatedSprite.flip_h = true
+	$AnimatedSprite.play("walk")
+	velocity.y += GRAVITY
+	velocity = move_and_slide(velocity, FLOOR)
+	if is_on_wall():
+		direction *= -1
