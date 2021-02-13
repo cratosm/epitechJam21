026e98c92e03d39ca9aa78838b68beda695d77b1
cratosm <matthias.virassamy@epitech.eu>
2021-02-13 11:27:58 +0100

    player shoot png and shoot

--- player/Player.gd
+++ player/Player.gd
@@ -10,19 +10,80 @@ const JUMP = -450
 var Jumps = 1#(+1)
 #MOVE
 var motion = Vector2()
+var stack = 0
+var fireball_power = 1
+var fireball = null
+const FIREBALL = preload("res://Objects/shoot.tscn")
+const FIREBALLUP = preload("res://Objects/FireballUp.tscn")
+const SHOOT = "ui_focus_next"
+var posTarget = null
+const MAXSTACK = 20
+var Dead = "Dead"
+var Run = "Run"
+var Idle = "Idle"
+var Jump = "Jump"
+var Fall = "Fall"
+var Die = "Die"
+var Idle_and_Shoot = "Idle and Shoot"
+var Run_and_Shoot = "Idle and Shoot"
+var Jump_and_Shoot = "Idle and Shoot"
+var buffer = ""
+var max_speed = 290
+
+func manageShoot():
+	if fireball_power == 1:
+		fireball = FIREBALL.instance()
+	elif fireball_power == 2:
+		fireball = FIREBALLUP.instance()
+	if sign(posTarget.position.x) == 1:
+		fireball.set_fireball_direction(1)
+	else:
+		fireball.set_fireball_direction(-1)
+	get_parent().add_child(fireball)
+	fireball.position =  posTarget.global_position
+	pass
 
 func manageRun():#RUN
-	if Input.is_action_pressed("ui_right"):
-		$Sprite.flip_h = false
-		$Sprite.play("Run")
-		motion.x = min(motion.x + ACCELERATION, MAX_SPEED);
-	elif Input.is_action_pressed("ui_left"):
+	if Input.is_action_just_pressed(SHOOT) && Input.is_action_pressed("ui_right"):
 		$Sprite.flip_h = true
-		$Sprite.play("Run")
-		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED);
+		if sign(posTarget.position.x) == -1:
+			posTarget.position.x *= -1
+			$Sprite/Cac/hitboxCac.position.x *= -1
+			$Sprite/Cac/PositionArm.position.x *= -1
+		stack = MAXSTACK
+		buffer = Run_and_Shoot
+		manageShoot()
+		motion.x = min(motion.x + ACCELERATION, max_speed);
+	elif Input.is_action_pressed("ui_right"):
+		$Sprite.flip_h = true
+		if sign(posTarget.position.x) == -1:
+			posTarget.position.x *= -1
+		$Sprite.play(Run)
+		motion.x = min(motion.x + ACCELERATION, max_speed);
+	elif Input.is_action_just_pressed(SHOOT) && Input.is_action_pressed("ui_left"):
+		$Sprite.flip_h = false
+		if sign(posTarget.position.x) == 1:
+			posTarget.position.x *= -1
+		stack = MAXSTACK
+		buffer = Run_and_Shoot
+		manageShoot()
+		motion.x = max(motion.x - ACCELERATION, -max_speed);
+	elif Input.is_action_pressed("ui_left"):
+		$Sprite.flip_h = false
+		if sign(posTarget.position.x) == 1:
+			posTarget.position.x *= -1
+		$Sprite.play(Run)
+		motion.x = max(motion.x - ACCELERATION, -max_speed);
 	else:
-		$Sprite.play("Idle")
-		motion.x = 0
+		$Sprite.play(Idle)
+		if Input.is_action_just_pressed(SHOOT):
+			stack = MAXSTACK
+			buffer = Idle_and_Shoot
+			manageShoot()
+		motion.x = lerp(motion.x, 0, 0.2)
+	if (stack != 0):
+		$Sprite.play(buffer)
+		stack -= 1
 	pass
 
 func manageJump():#JUMP
@@ -32,17 +93,27 @@ func manageJump():#JUMP
 			motion.y = JUMP
 	else:
 		if motion.y > 0:
-			$Sprite.play("Fall")
+			$Sprite.play(Fall)
 		else:
-			$Sprite.play("Jump")
-
-	if Input.is_action_just_pressed("ui_up") && Jumps != 0:
-		motion.y = JUMP
-		#Jumps -= 1
+			$Sprite.play(Jump)
+			if Input.is_action_just_pressed(SHOOT):
+				stack = MAXSTACK
+				buffer = Jump_and_Shoot
+				manageShoot()
+		motion.x = lerp(motion.x, 0, 0.05)
+		if Input.is_action_just_pressed("ui_up") && Jumps != 0:
+			motion.y = JUMP
+			Jumps -= 1
+	if (stack > 0):
+		$Sprite.play(buffer)
+		stack -= 1
+	else:
+		buffer = ""
 	pass
 
 func _physics_process(delta):#MAIN
 	motion.y += GRAVITY
+	posTarget = $Position2D
 	manageRun()
 	manageJump()
 	motion = move_and_slide(motion, UP)
