5b214e8c2445c1d6cac6d46a12dfecb550a84871
Yoann LE PECH <51167167+Yoannlp44@users.noreply.github.com>
2021-02-13 16:57:06 +0100

    add portal script

---
+++ Objects/Portal.gd
@@ -0,0 +1,19 @@
+extends Area2D
+export(PackedScene) var target_scene
+
+func _ready():
+	pass
+	
+	
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		if !target_scene:
+			print("no scene load")
+			return
+		if get_overlapping_bodies().size() > 1:
+			next_level()
+
+func next_level():
+	var ERR = get_tree().change_scene_to(target_scene)
+	if ERR != OK:
+		print("error")
